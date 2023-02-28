8d567527bd4b71ac8fb5a38cb13e5fb5a2cce42b
Standard <oanlara8@gmail.com>
2023-02-27 17:06:26 -0700

    timestamps

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ In a mobile application, the notifications section is typically not a drop-down 
 Making it mobile-friendly would involve changing the spec for the notifications component entirely.
 
 Implementing the notifications on mobile would mostly involve copying 80% of the code already written, but changing widths and text overflow thresholds.
+
+# Notifications
+
+This code was written with the assumption that:
+
+1. Notifications would be returned from the backend in chronological order.
+
+In the case that it isn't, sorting the notifications would require a simple statement: notifications.sort((a, b) => b.timestamp - a.timestamp);
+
+2. The backend would return only the X most recent notifications.
+
+If not, simply truncate the notifications object with: if (notifications.length > 20) notifications = notifications.slice(0, 20);
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,32 +6,75 @@ const notifications = [
   {
     message: "Hey, I just met you",
     user: "Carly",
-    timestamp: "2 minutes ago",
+    timestamp: Date.now(),
     type: "reply",
     read: false,
   },
   {
     message: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.",
     user: "Jerry",
-    timestamp: "3 minutes ago",
+    timestamp: (Date.now()-60000*((Math.floor(Math.random() * 9) + 1))),
     type: "comment",
     read: false,
   },
   {
     message: "The Industrial Revolution and its consequences have been a disaster for the human race. They have greatly increased the life-expectancy of those of us who live in “advanced” countries, but they have destabilized society, have made life unfulfilling, have subjected human beings to indignities, have led to widespread psychological suffering (in the Third World to physical suffering as well) and have inflicted severe damage on the natural world.",
     user: "Ted",
-    timestamp: "4 minutes ago",
+    timestamp: (Date.now()-3600000*(Math.floor(Math.random() * 9) + 1)),
     type: "reply",
     read: false,
   },
   {
     message: "Oh wow the aliens are coming for me",
     user: "Cixin",
-    timestamp: "5 minutes ago",
+    timestamp: (Date.now()-86400000*(Math.floor(Math.random() * 9) + 1)),
     type: "comment",
     read: true
   },
 ];
+
+let timePhrase = (providedUnixtime) => {
+  let unixTime = Date.now()-providedUnixtime
+  // console.log(Date(dateTimeString))
+
+  if (unixTime >= 31536000000) {
+    return "Over a year ago"
+  }
+  if (unixTime >= 2628000000) {
+      let phrase = "month"
+      let properUnit = (Math.round(unixTime / 2628000000))
+      if (properUnit >= 2) phrase = "months"
+      return `${properUnit} ${phrase} ago`
+  }
+  if (unixTime >= 604800000) {
+      let phrase = "week"
+      let properUnit = (Math.round(unixTime / 604800000))
+      if (properUnit >= 2) phrase = "weeks"
+      return `${properUnit} ${phrase} ago`
+  }
+  if (unixTime >= 86400000) {
+      let phrase = "day"
+      let properUnit = (Math.round(unixTime / 86400000))
+      if (properUnit >= 2) phrase = "days"
+      return `${properUnit} ${phrase} ago`
+  }
+  else if (unixTime >= 3600000) {
+      let phrase = "hour"
+      let properUnit = (Math.round(unixTime / 3600000))
+      if (properUnit >= 2) phrase = "hours"
+      return `${properUnit} ${phrase} ago`
+  }
+  else if (unixTime >= 60000) {
+      let phrase = "minute"
+      let properUnit = (Math.round(unixTime / 60000))
+      if (properUnit >= 2) phrase = "minutes"
+      return `${properUnit} ${phrase} ago`
+  }
+  else {
+      return "Moments ago"
+  }
+
+}
 
 function detailedNotifications() {
   return (
@@ -47,7 +90,7 @@ function detailedNotifications() {
 
       const notification_item = (
         <div className={notification_class}>
-          <span className='notification-timestamp'>{notification.timestamp}</span>
+          <span className='notification-timestamp'>{timePhrase(notification.timestamp)}</span>
           <span className='notification-text'>{notification_text}</span>
         </div>
       )
